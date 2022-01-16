6b963553cdd33f5175551add5ce2e69b0053f405
KrzysztofNewski <65493878+KrzysztofNewski@users.noreply.github.com>
2022-01-16 20:18:37 +0100

    Projekt

---
+++ Core/Src/main.c
@@ -0,0 +1,321 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "adc.h"
+#include "i2c.h"
+#include "tim.h"
+#include "usart.h"
+#include "usb_otg.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdio.h"
+#include "bh1750_config.h"
+#include "PID.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+int value_PV;
+int value_SP=10;
+int value_U;
+pid_str*PID;
+float kp_init=4.38127169718089; //1.5
+float ki_init=0.355762323613465; //0.5
+float kd_init=0;
+int anti_windup_limit_init=20;
+int __io_putchar(int ch)
+{
+	if(ch=='\n')
+	{
+		int ch2='\r';
+		HAL_UART_Transmit(&huart3, (uint8_t*)&ch2, 1, HAL_MAX_DELAY);
+
+	}
+	HAL_UART_Transmit(&huart3, (uint8_t*)&ch, 1, HAL_MAX_DELAY);
+	return ch;
+}
+int value_int=0;
+void zadanie3_4()
+{
+	 HAL_Delay(100);
+	 value_int=BH1750_ReadLux(&hbh1750_1);
+	 printf("LUXY: %d\n",value_int); // to do 4
+}
+uint8_t komunikat[3];
+int liczba=0;
+void zadanie5()
+{
+	if(HAL_UART_Receive(&huart3, komunikat, 3, HAL_MAX_DELAY)==HAL_OK)
+	{
+        sscanf(komunikat,"%d",&liczba);
+        printf("LICZBA TO: %d\n",liczba);
+        __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, liczba*10);
+        HAL_Delay(200);
+        float value=BH1750_ReadLux(&hbh1750_1);
+        printf("LUXY: %.3f\n",value);
+
+	}
+}
+int czas=0;
+void zadanie6()
+{
+	 HAL_Delay(100);
+	 float value=BH1750_ReadLux(&hbh1750_1);
+	 printf("LUX %.3f,PWM: %d\n",value,czas);
+	__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, czas);
+		  czas=czas+25;
+		 	 if(czas>1000)
+		 	 {
+		 		 czas=0;
+
+		 	 }
+}
+
+//PID
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  if (htim == &htim6)
+  {
+	 //SP
+	 HAL_ADC_Start(&hadc1);
+	 HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
+	 uint32_t adc=HAL_ADC_GetValue(&hadc1);
+	 value_SP=70*adc/4096;
+
+	 //PV
+	 value_PV=BH1750_ReadLux(&hbh1750_1);
+	 printf("%d %d\n",value_PV,value_SP);
+	 value_U=pid_calculate(PID, value_SP, value_PV);
+	 int PWM=__HAL_TIM_GET_COMPARE(&htim3,TIM_CHANNEL_1);
+	 PWM=PWM+value_U;
+	 if(PWM<0)
+	 {
+		 PWM=0;
+	 }
+	 __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, PWM);
+	// printf("SYGNAL STERUAJCY: %d\n",value_U);
+	// printf("PWM: %d\n",PWM);
+	 HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+  }
+}
+/*void zadanie_pid()
+{
+	HAL_Delay(1000);
+	value_PV=BH1750_ReadLux(&hbh1750_1);
+	printf("%d\n",value_PV);
+	//printf("SP: %d\n",value_SP);
+}*/
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART3_UART_Init();
+  MX_USB_OTG_FS_PCD_Init();
+  MX_I2C1_Init();
+  MX_TIM3_Init();
+  MX_TIM6_Init();
+  MX_ADC1_Init();
+  /* USER CODE BEGIN 2 */
+  BH1750_Init(&hbh1750_1);
+  HAL_TIM_Base_Start_IT(&htim3);
+  HAL_TIM_Base_Start_IT(&htim6);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+  //pid
+  pid_init(PID,kp_init,ki_init,kd_init,anti_windup_limit_init);
+  //pid_reset(PID);
+
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	 // HAL_Delay(100);
+	 // float value=BH1750_ReadLux(&hbh1750_1);
+	//  printf("LUX: %.3f,PWM: %d\n",value,czas); zadanie 6
+
+	 // printf("LUXY: %.3f\n",value);
+
+	  //zadanie6();
+
+
+
+
+
+
+
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+
+  /** Configure LSE Drive Capability
+  */
+  HAL_PWR_EnableBkUpAccess();
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 72;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 3;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USART3|RCC_PERIPHCLK_I2C1
+                              |RCC_PERIPHCLK_CLK48;
+  PeriphClkInitStruct.Usart3ClockSelection = RCC_USART3CLKSOURCE_PCLK1;
+  PeriphClkInitStruct.I2c1ClockSelection = RCC_I2C1CLKSOURCE_PCLK1;
+  PeriphClkInitStruct.Clk48ClockSelection = RCC_CLK48SOURCE_PLL;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
